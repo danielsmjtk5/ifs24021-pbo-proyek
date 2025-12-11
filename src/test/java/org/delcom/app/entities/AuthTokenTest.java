bfa9593e315f7c1024a8e2434dfef7b1a9265887
Daniel Simanjuntak <danielvsmjtk05@gmail.com>
2025-12-11 14:49:59 +0700

    Update AuthTokenTest.java

--- src/test/java/org/delcom/app/entities/AuthTokenTest.java
+++ src/test/java/org/delcom/app/entities/AuthTokenTest.java
@@ -1,17 +1,18 @@
 package org.delcom.app.entities;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName; // Import Wajib
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AuthTokenTest {
 
     @Autowired
@@ -56,7 +57,7 @@ class AuthTokenTest {
         // Saat ini ID masih null
         assertThat(authToken.getId()).isNull();
 
-        // 2. Simpan ke Database Virtual (H2)
+        // 2. Simpan ke Database
         // persistFlushFind = simpan, flush, lalu ambil ulang dari DB
         AuthToken savedToken = entityManager.persistFlushFind(authToken);
 
